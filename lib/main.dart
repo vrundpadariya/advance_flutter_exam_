d60e172ea5fa8dcea734964b744fdd50bb7ac3e0
Vrundpadariya <133338321+vrundpadariya@users.noreply.github.com>
2024-05-20 11:25:02 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/views/homepage/homepage.dart';
+import 'app/views/like_notes/views/likeNotes.dart';
 import 'app/views/splash_screen/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main(){
-  runApp(){
-   GetMaterialApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(useMaterial3: true),
     getPages: [
-      GetPage(name: '/', page: () => SplashScreen(),),
-      GetPage(name: '/home', page: () => homepage(),),
+      GetPage(
+        name: '/',
+        page: () => SplashScreen(),
+      ),
+      GetPage(
+        name: '/home',
+        page: () => homepage(),
+      ),
+      GetPage(
+        name: '/like',
+        page: () => likeNotes(),
+      ),
     ],
-  );
-  };
+  ));
 }
